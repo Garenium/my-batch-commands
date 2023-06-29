753fdf149e85c172fbaa157db756695f9a6d3ffc
Garen Ikezian <gifanshawe@gmail.com>
2023-06-29 10:07:58 -0400

    Create expl.bat

---
+++ expl/expl.bat
@@ -0,0 +1,4 @@
+@echo off
+:: For Windows 11 users that want to bring the old explorer back... the quickest way is by having this script in your bin directory
+:: replace YOUR_USERNAME to your user name from your command prompt
+start C:\Users\YOUR_USERNAME\AppData\Roaming\Microsoft\Windows\"Start Menu"\Programs\"Administrative Tools".lnk
